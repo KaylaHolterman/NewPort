52f0997362432fbc9108211cd074c9ebdce35b1f
Kayla Holterman <kpholterman@gmail.com>
2016-12-02 22:44:44 -0500

    hi

--- script1.js
+++
@@ -1,6 +0,0 @@
-$('#clickable_div').mouseover( function(){
-    $('#nav_menu').slideDown();
-})
-$('#wrap').mouseleave( function(){
-    $('#nav_menu').slideUp();
-});
--- script2.js
+++ script2.js
@@ -6,3 +6,23 @@ function closeNav() {
     document.getElementById("navi").style.width = "0%";
 }
 
+ 
+   $(document).ready(function() {
+	$("img").on("click", function(){
+	});
+	
+	$("a").on("click",function(){
+
+		
+var imageLink = $(this).data("img-url");
+
+   	$("#lightbox-img").attr('src', imageLink);
+
+  	$(".lightboxEx").show();
+
+  	$(".lightboxEx").on("click",function(){
+      $(".lightboxEx").hide();
+ });
+   });
+     });
+
